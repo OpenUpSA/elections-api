4d1f0d06ad34bffd8b43af7e0594eb9aa765eaaf
Adi Eyal <adi@burgercom.co.za>
2013-11-06 05:15:17 +0200

    Added deployment script

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,10 @@
+from fabdefs import *
 from fabric.operations import local, run
 from fabric.context_managers import lcd
 import os
+
+python = "%s/bin/python" % env_dir
+pip = "%s/bin/pip" % env_dir
 
 elections_url = "http://www.elections.org.za/content/uploadedfiles/2009%20NPE.zip"
 elections_file = "2009 NPE.csv"
@@ -21,3 +25,13 @@ def setup_db():
     with lcd(project_root):
         local("python manage.py syncdb")
         local("python manage.py loaddata '../data/{}'".format(elections_file))
+
+def deploy():
+    with api.cd(code_dir):
+        api.run("git pull origin master")
+        api.run("%s install -r %s/requirements/production.txt --quiet" % (pip, code_dir))
+
+        with api.cd(os.path.join(code_dir, project_root)):
+            api.run("%s manage.py collectstatic --noinput --settings=settings.production" % python)
+
+        api.sudo("supervisorctl restart iec")
