9e54b7f304ef002f2446e769bd468f6e6877cd83
Adi Eyal <adi@burgercom.co.za>
2013-11-06 09:39:50 +0200

    Updated production settings

--- server/settings/production.py
+++ server/settings/production.py
@@ -1,6 +1,37 @@
+"""Development settings and globals."""
+
+from os.path import join, normpath
+
 from base import *
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ["localhost", "iec.code4sa.org"]
+########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = False
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
+
+########## EMAIL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+########## END EMAIL CONFIGURATION
+
+
+########## DATABASE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': normpath(join(DJANGO_ROOT, 'default.db')),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+########## END DATABASE CONFIGURATION
+
+ALLOWED_HOSTS = ["iec.code4sa.org"]
