e0707a290b692e57dac57b9e08b089e90b5fb672
Adi Eyal <adi@burgercom.co.za>
2013-11-06 11:33:25 +0200

    Results can now be filtered by geographical location

--- server/iec/views.py
+++ server/iec/views.py
@@ -52,9 +52,49 @@ class ResultViewSet(viewsets.ModelViewSet):
     queryset = models.Result.objects.all()
     serializer_class = serializers.ResultSerializer
 
+    def get_queryset(self):
+        queryset = models.Result.objects.all()
+        voting_district = self.request.QUERY_PARAMS.get('voting_district', None)
+        ward = self.request.QUERY_PARAMS.get('ward', None)
+        municipality = self.request.QUERY_PARAMS.get('municipality', None)
+        province = self.request.QUERY_PARAMS.get('province', None)
+
+        if voting_district is not None:
+            queryset = queryset.filter(voting_district__code=voting_district)
+
+        if ward is not None:
+            queryset = queryset.filter(voting_district__ward__code=ward)
+
+        if municipality is not None:
+            queryset = queryset.filter(voting_district__ward__municipality__pk=municipality)
+
+        if province is not None:
+            queryset = queryset.filter(voting_district__ward__municipality__province__pk=province)
+        return queryset
+
 class ResultSummaryViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows a result summary to be viewed or edited.
     """
     queryset = models.ResultSummary.objects.all()
     serializer_class = serializers.ResultSummarySerializer
+
+    def get_queryset(self):
+        queryset = models.ResultSummary.objects.all()
+        voting_district = self.request.QUERY_PARAMS.get('voting_district', None)
+        ward = self.request.QUERY_PARAMS.get('ward', None)
+        municipality = self.request.QUERY_PARAMS.get('municipality', None)
+        province = self.request.QUERY_PARAMS.get('province', None)
+
+        if voting_district is not None:
+            queryset = queryset.filter(voting_district__code=voting_district)
+
+        if ward is not None:
+            queryset = queryset.filter(voting_district__ward__code=ward)
+
+        if municipality is not None:
+            queryset = queryset.filter(voting_district__ward__municipality__pk=municipality)
+
+        if province is not None:
+            queryset = queryset.filter(voting_district__ward__municipality__province__pk=province)
+        return queryset
