2e3352c7908ad13b14b6289d4053181534e1db12
Adi Eyal <adi@burgercom.co.za>
2013-11-24 11:43:58 +0200

    Added various filters to api views

--- server/iec/views.py
+++ server/iec/views.py
@@ -1,33 +1,24 @@
 import models
 from rest_framework import viewsets
+from rest_framework import filters
+from rest_framework import generics
+import django_filters
 import serializers
 
-class EventViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows an event to be viewed or edited.
-    """
+class EventViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Event.objects.all()
     serializer_class = serializers.EventSerializer
 
-class PartyViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows party to be viewed or edited.
-    """
+class PartyViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Party.objects.all()
     serializer_class = serializers.PartySerializer
 
-
-class ProvinceViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows a province to be viewed or edited.
-    """
+class ProvinceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Province.objects.all()
     serializer_class = serializers.ProvinceSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
 
-class MunicipalityViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows a municipality to be viewed or edited.
-    """
+class MunicipalityViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.MunicipalitySerializer
     queryset = models.Municipality.objects.all()
 
@@ -39,10 +30,7 @@ class MunicipalityViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(province__name__iexact=province)
         return queryset
 
-class WardViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows a ward to be viewed or edited.
-    """
+class WardViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Ward.objects.all()
     serializer_class = serializers.WardSerializer
 
@@ -58,10 +46,7 @@ class WardViewSet(viewsets.ModelViewSet):
 
         return queryset
 
-class VotingDistrictViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows a voting district to be viewed or edited.
-    """
+class VotingDistrictViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.VotingDistrict.objects.all()
     serializer_class = serializers.VotingDistrictSerializer
 
@@ -80,56 +65,63 @@ class VotingDistrictViewSet(viewsets.ModelViewSet):
 
         return queryset
 
-class ResultViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows result to be viewed or edited.
-    """
+class ResultFilter(django_filters.FilterSet):
+    voting_district = django_filters.CharFilter(name="voting_district__code")
+    ward = django_filters.CharFilter(name="voting_district__ward__code")
+    municipality = django_filters.CharFilter(name="voting_district__ward__municipality__pk")
+    province = django_filters.CharFilter(name="voting_district__ward__municipality__province__name", lookup_type="iexact")
+    party = django_filters.CharFilter(name="party__name", lookup_type="iexact")
+
+    max_votes = django_filters.NumberFilter(name="votes", lookup_type="lte")
+    min_votes = django_filters.NumberFilter(name="votes", lookup_type="gte")
+
+    class Meta:
+        model = models.Result
+        fields = [
+            'votes', 'max_votes', 'min_votes',
+            'party',
+            'voting_district', 'ward', 'municipality', 'province'
+        ]
+
+class ResultViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Result.objects.all()
     serializer_class = serializers.ResultSerializer
+    filter_class = ResultFilter
 
-    def get_queryset(self):
-        queryset = models.Result.objects.all()
-        voting_district = self.request.QUERY_PARAMS.get('voting_district', None)
-        ward = self.request.QUERY_PARAMS.get('ward', None)
-        municipality = self.request.QUERY_PARAMS.get('municipality', None)
-        province = self.request.QUERY_PARAMS.get('province', None)
 
-        if voting_district is not None:
-            queryset = queryset.filter(voting_district__code=voting_district)
+class ResultSummaryFilter(django_filters.FilterSet):
+    voting_district = django_filters.CharFilter(name="voting_district__code")
+    ward = django_filters.CharFilter(name="voting_district__ward__code")
+    municipality = django_filters.CharFilter(name="voting_district__ward__municipality__pk")
+    province = django_filters.CharFilter(name="voting_district__ward__municipality__province__name", lookup_type="iexact")
 
-        if ward is not None:
-            queryset = queryset.filter(voting_district__ward__code=ward)
+    max_total_votes = django_filters.NumberFilter(name="total_votes", lookup_type="lte")
+    min_total_votes = django_filters.NumberFilter(name="total_votes", lookup_type="gte")
+    max_spoilt_votes = django_filters.NumberFilter(name="spoilt_votes", lookup_type="lte")
+    min_spoilt_votes = django_filters.NumberFilter(name="spoilt_votes", lookup_type="gte")
+    max_registered_voters = django_filters.NumberFilter(name="registered_voters", lookup_type="lte")
+    min_registered_voters = django_filters.NumberFilter(name="registered_voters", lookup_type="gte")
+    max_special_votes = django_filters.NumberFilter(name="special_votes", lookup_type="lte")
+    min_special_votes = django_filters.NumberFilter(name="special_votes", lookup_type="gte")
+    max_voter_turnout_perc = django_filters.NumberFilter(name="voter_turnout_perc", lookup_type="lte")
+    min_voter_turnout_perc = django_filters.NumberFilter(name="voter_turnout_perc", lookup_type="gte")
+    max_section_24a_votes = django_filters.NumberFilter(name="section_24a_votes", lookup_type="lte")
+    min_section_24a_votes = django_filters.NumberFilter(name="section_24a_votes", lookup_type="gte")
 
-        if municipality is not None:
-            queryset = queryset.filter(voting_district__ward__municipality__pk=municipality)
+    class Meta:
+        model = models.ResultSummary
+        fields = [
+            'total_votes', 'max_total_votes', 'min_total_votes',
+            'spoilt_votes', 'max_spoilt_votes', 'min_spoilt_votes',
+            'registered_voters', 'max_registered_voters', 'min_registered_voters',
+            'special_votes', 'max_special_votes', 'min_special_votes',
+            'voter_turnout_perc', 'max_voter_turnout_perc', 'min_voter_turnout_perc',
+            'section_24a_votes', 'max_section_24a_votes', 'min_section_24a_votes',
+            'voting_district', 'ward', 'municipality', 'province'
+        ]
 
-        if province is not None:
-            queryset = queryset.filter(voting_district__ward__municipality__province__pk=province)
-        return queryset
+class ResultSummaryViewSet(viewsets.ReadOnlyModelViewSet):
 
-class ResultSummaryViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows a result summary to be viewed or edited.
-    """
     queryset = models.ResultSummary.objects.all()
     serializer_class = serializers.ResultSummarySerializer
-
-    def get_queryset(self):
-        queryset = models.ResultSummary.objects.all()
-        voting_district = self.request.QUERY_PARAMS.get('voting_district', None)
-        ward = self.request.QUERY_PARAMS.get('ward', None)
-        municipality = self.request.QUERY_PARAMS.get('municipality', None)
-        province = self.request.QUERY_PARAMS.get('province', None)
-
-        if voting_district is not None:
-            queryset = queryset.filter(voting_district__code=voting_district)
-
-        if ward is not None:
-            queryset = queryset.filter(voting_district__ward__code=ward)
-
-        if municipality is not None:
-            queryset = queryset.filter(voting_district__ward__municipality__pk=municipality)
-
-        if province is not None:
-            queryset = queryset.filter(voting_district__ward__municipality__province__name__iexact=province)
-        return queryset
+    filter_class = ResultSummaryFilter
