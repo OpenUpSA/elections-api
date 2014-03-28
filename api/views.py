8ed4645bf7a3b9b076cf7cb9c6da93d803afeb59
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-03-28 11:42:41 +0200

    Revert "Allow for cross-domain access for API"
    This reverts commit 6ca75dbbceb58a0cfe73da435aced29e07b35067.

--- api/views.py
+++ api/views.py
@@ -1,11 +1,9 @@
 from api import app, logger, db
-from flask import jsonify, request, make_response, render_template, redirect, current_app
+from flask import jsonify, request, make_response, render_template, redirect
 from models import *
 from serializers import serialize_area
 import json
 import time
-from datetime import timedelta 
-from functools import update_wrapper
 from sqlalchemy.sql import func
 
 HOST = app.config['HOST']
@@ -14,46 +12,6 @@ event_types = ["provincial", "national"]
 years = [1999, 2004, 2009]
 areas = ["province", "municipality", "ward", "voting_district"]
 
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
 
 class ApiException(Exception):
     """
@@ -114,7 +72,6 @@ def validate_area(area):
 
 
 @app.route('/')
-@crossdomain(origin='*')
 def index_event_types():
     """
     Landing page. Return links to available event_types.
@@ -127,7 +84,6 @@ def index_event_types():
 
 
 @app.route('/<event_type>/')
-@crossdomain(origin='*')
 def index_years(event_type):
     """
     Return links to available years.
@@ -141,7 +97,6 @@ def index_years(event_type):
 
 
 @app.route('/<event_type>/<year>/')
-@crossdomain(origin='*')
 def results_overall(event_type, year):
     """
     Return overall national results, with links to available areas.
@@ -162,7 +117,6 @@ def results_overall(event_type, year):
 
 @app.route('/<event_type>/<year>/<area>/')
 @app.route('/<event_type>/<year>/<area>/<area_id>/')
-@crossdomain(origin='*')
 def results_by_area(event_type, year, area, area_id=None):
     """
     Return results for the specified area, with links to available parent areas where applicable.
