ab6318eeaded2e8f72aff1cdc8c802ded499205e
root <root@prod-elections-api>
2014-05-07 20:13:43 +0000

    Fixed merge

--- instance/config.py
+++ instance/config.py
@@ -4,7 +4,7 @@ LOG_LEVEL = logging.DEBUG
 LOGGER_NAME = "iec_api_logger"  # make sure this is not the same as the name of the package to avoid conflicts with Flask's own logger
 DEBUG = True
 
-HOST = "http://localhost:5000"
+HOST = "http://election-api.code4sa.org"
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/tmp.db'
 
